61df23fbf28a0fa3698ed3bf2e2ee5bd77664493
Robert Giles <robert.ryan.giles@gmail.com>
2016-12-06 23:38:04 -0600

    Made progress on easy challenge 2
    Made progress on easy challenge 2

--- r_dailyprogrammer easy challenge 2/r_dailyprogrammer daily challenge 2/main.cpp
+++ r_dailyprogrammer easy challenge 2/r_dailyprogrammer daily challenge 2/main.cpp
@@ -15,11 +15,63 @@ EXTRA EXTRA CREDIT: put all output into a log file
 #include <fstream>
 using namespace std;
 
+void calcForce();
+void calcMass();
+void calcAcceleration();
+
 int main()
 {
 	ofstream log; //outputs to a log file
 
-	float 
+	log.open("log.txt"); //open output stream to log.txt
+	log << "log.txt\n\n";
 
+	float force;
+	float mass;
+	float acceleration;
+
+	char userChoice;
+
+	do { //loop until the user is done
+		cout << "What would you like to calculate? Enter 'F' for Force, 'M' for mass, or 'A' for acceleration. (Enter X to exit)\n"; //prompt user
+		cin >> userChoice; //receive user input
+
+		switch (userChoice)
+		{
+		case 'F':
+			calcForce();
+			break;
+
+		case 'M':
+			calcMass();
+			break;
+
+		case 'A':
+			calcAcceleration();
+			break;
+
+		default:
+			break;
+
+		}
+	} while (userChoice != 'X');
+
+	
+	
 	return 0;
+}
+
+void calcForce()
+{
+	return;
+}
+
+void calcMass()
+{
+	return;
+}
+
+void calcAcceleration()
+{
+	return;
 }
