64e42bfcee36b3bee8d9e61f1738a6f83f266b23
Robert Giles <robert.ryan.giles@gmail.com>
2016-12-07 20:28:37 -0600

    Finished easy challenge 2
    Implemented calculate functions and finished the challenge

--- r_dailyprogrammer easy challenge 2/r_dailyprogrammer daily challenge 2/main.cpp
+++ r_dailyprogrammer easy challenge 2/r_dailyprogrammer daily challenge 2/main.cpp
@@ -7,71 +7,89 @@ For example, if you were in physics class, you might want to make a F = M * A ca
 
 EXTRA CREDIT: make the calculator have multiple functions! Not only should it be able to calculate F = M * A, but also A = F/M, and M = F/A!
 
-EXTRA EXTRA CREDIT: put all output into a log file
-
 */
 
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-void calcForce();
-void calcMass();
-void calcAcceleration();
+double calcForce(double, double);
+double calcMass(double, double);
+double calcAcceleration(double, double);
 
 int main()
 {
-	ofstream log; //outputs to a log file
+	double force = 0; //store force
+	double mass = 0; //store mass
+	double acceleration = 0; //store acceleration
 
-	log.open("log.txt"); //open output stream to log.txt
-	log << "log.txt\n\n";
-
-	float force;
-	float mass;
-	float acceleration;
-
-	char userChoice;
+	char userChoice; //store user choice at the "menu"
 
 	do { //loop until the user is done
 		cout << "What would you like to calculate? Enter 'F' for Force, 'M' for mass, or 'A' for acceleration. (Enter X to exit)\n"; //prompt user
 		cin >> userChoice; //receive user input
+		
+		cout << "\n---------------------------------------------------------------\n";
 
-		switch (userChoice)
+		switch (userChoice) //call a calculate function based on the user's choice
 		{
+		case 'f':
 		case 'F':
-			calcForce();
+			cout << "Enter Mass: ";
+			cin >> mass;
+			cout << "Enter Acceleration: ";
+			cin >> acceleration;
+			cout << "\nForce = " << calcForce(mass, acceleration) << endl << endl; //call calculate force function
 			break;
 
+		case 'm':
 		case 'M':
-			calcMass();
+			cout << "Enter Force: ";
+			cin >> force;
+			cout << "Enter Acceleration: ";
+			cin >> acceleration;
+			cout << "\nMass: " << calcMass(force, acceleration) << endl << endl; //call calculate mass function
 			break;
 
+		case 'a':
 		case 'A':
-			calcAcceleration();
+			cout << "Enter Force: ";
+			cin >> force;
+			cout << "Enter mass: ";
+			cin >> mass;
+			cout << "\nAcceleration: " << calcAcceleration(force, mass) << endl << endl; //call calculate acceleration function
+			break;
+
+		case 'x':
+		case 'X':
 			break;
 
 		default:
+			cout << "\ninvalid option\n";
 			break;
 
 		}
-	} while (userChoice != 'X');
+	} while (userChoice != 'X' && userChoice != 'x');
 
-	
+	cout << "\n---------------------------------------------------------------\n";
 	
 	return 0;
 }
 
-void calcForce()
+//returns F = M * A
+double calcForce(double mass, double acceleration)
 {
-	return;
+	return mass * acceleration;
 }
 
-void calcMass()
+//returns M = F / A
+double calcMass(double force, double acceleration)
 {
-	return;
+	return force / acceleration;
 }
 
-void calcAcceleration()
+//returns A = F / M
+double calcAcceleration(double force, double mass)
 {
-	return;
+	return force / mass;
 }
